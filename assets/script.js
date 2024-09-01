161f7505fd1be8a56e514e3e8afd9f17a352f80d
msyfk <143171049+msyfk@users.noreply.github.com>
2024-09-01 12:54:28 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#element", {
+  strings: [
+    "Web Developer", 
+    "Web Designer", 
+    "Freelancer", 
+    "Grapich Designer", 
+    "UI/UX Designer",
+  ],
+  typeSpeed: 100,
+  loop: true,
+  shuffle: true
+})
